538557c505e009c489e5541e02b837ca917d5566
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-09-18 00:15:13 +0300

    last

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const inputText = document.querySelector("input");
 const btnSearch = document.querySelector('.button');
 const galleryEl = document.querySelector('.gallery');
 const loadMoreEl = document.querySelector('.js-load-more');
-let page = 1;
+
 
 function errorMsg() {
   Notify.failure('Sorry, there are no images matching your search query. Please try again.', {
@@ -23,6 +23,15 @@ function errorMsg() {
     fontSize: '24px'});
 }
 
+function infoMsg() {
+  Notify.failure("We are sorry, but you've reached the end of search results.", {
+    position: 'center-center',
+    timeout: 5000,
+    width: '400px',
+    fontSize: '24px'});
+}
+
+
 
 
 const getImages = async (data, page) => {
@@ -30,15 +39,14 @@ const getImages = async (data, page) => {
     const listImg = await makeRequest(data, page);
 
     if (listImg) {
-        console.log(listImg);
+        console.log(listImg, listImg.totalHits);
         if (listImg.total === 0 ) {
           errorMsg();
           return ;
   
         }
         renderImages(listImg.hits);
-
-
+        
         // toggleLoader(loader, "hide");
         // return listImg;
       }
@@ -82,50 +90,50 @@ function renderImages(images) {
         per_page: 40,
         page,
       });
-      
+      const endpoint = BASE_URL + "?" + searchParams.toString();
     //   console.log("serchParams:", searchParams.toString());
-    try {
-      const result = await fetch(BASE_URL + "?" + searchParams.toString())
-      loadMoreEl.classList.replace("load-more-hidden", "load-more");
-        if (result.ok) {
-        // console.log(result.json())
-        // if (result.page === result.totalHits) {
-        //   loadMoreEl.classList.replace("load-more", "load-more-hidden");
-        //   }
+    // try {
+    //   const result = await fetch(BASE_URL + "?" + searchParams.toString())
+    //   loadMoreEl.classList.replace("load-more-hidden", "load-more");
+    //     if (result.ok) {
+    //     // console.log(result.json())
+    //     // if (result.page === result.totalHits) {
+    //     //   loadMoreEl.classList.replace("load-more", "load-more-hidden");
+    //     //   }
           
-        return result.json();
-      }
+    //     return result.json();
+    //   }
       
-      throw new Error(result.statusText);
+    //   throw new Error(result.statusText);
       
-    } catch (error) {
-       errorMsg();
-        console.log(err)
+    // } catch (error) {
+    //    errorMsg();
+    //     console.log(err)
         
-    }
+    // }
   
-//     return axios.get(`${BASE_URL}?${searchParams.toString()}`)
-//       .then((res) => {
-//         if (res.ok) {
-//             console.log(res.json())
-//           return res.json();
-//         }
-//         throw new Error(res.statusText);
-//       }).then((res) => {
-//               loadMoreEl.classList.replace("load-more-hidden", "load-more");
+    return fetch(endpoint)
+      .then((res) => {
+        if (res.status === 200) {
+                  return res.json();
+                }
         
-//               if (res.page === res.total) {
-//                 loadMoreEl.classList.replace("load-more", "load-more-hidden");
-//               }
-//               return res;
-//             })
-//       .catch((err) => {Notify.failure('Sorry, there are no images matching your search query. Please try again.', {
-//         position: 'center-center',
-//         timeout: 5000,
-//         width: '400px',
-//         fontSize: '24px'});
-//     console.log(err)
-// });
+        // if (res.ok) {
+        //     console.log(res.json())
+        //   // return res.json();
+        // }
+        throw new Error(res.statusText);
+      })
+      .then((res) => {
+              loadMoreEl.classList.replace("load-more-hidden", "load-more");
+              console.log(page * res.hits.length);
+              if ((page * res.hits.length) >= res.totalHits) {
+                loadMoreEl.classList.replace("load-more", "load-more-hidden");
+                infoMsg();
+              }
+              return res;
+            })
+      .catch((err) => errorMsg());
   };
 
   // *********************** Кнопка "Load More" ************************** \\
@@ -133,27 +141,49 @@ function renderImages(images) {
  
 
 
-const searchInfoCallback = (e) => {
+const searchInfoCallback = async (e) => {
     e.preventDefault();
+    let page = 1;
     const formData = new FormData(e.target);
     const data = Object.fromEntries(formData);
+    e.currentTarget.reset();
     getImages (data, page);
-    const loadMoreHandler = async () => {
+ 
+      const loadMoreHandler = () => {
       page += 1;
       getImages (data, page);
       };
-    
+      loadMoreEl.onclick = () => {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
       loadMoreEl.addEventListener("click", loadMoreHandler);
-    e.currentTarget.reset();
-    
+      const totHits = await makeRequest(data, page);
+      Notify.info(`Hooray! We found ${totHits.totalHits} images.`);
   };
 
-  userForm.addEventListener("submit", searchInfoCallback);
-//   let gallery = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250});
+//  let gallery = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250});
     
-//   gallery.on('show.simplelightbox', function () {
+//     gallery.on('show.simplelightbox', function () {
       
-// console.log(gallery);
-// });
+//     console.log(gallery);
+//     });
+
+
+ userForm.addEventListener("submit", searchInfoCallback);
+ const { height: cardHeight } = document
+ .querySelector(".gallery")
+ .firstElementChild.getBoundingClientRect();
+
+ window.scrollBy({
+ top: cardHeight * 2,
+ behavior: "smooth",
+});
+
+
+  
 
 
